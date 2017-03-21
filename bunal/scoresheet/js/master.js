dda76f2a0c0ae871bfed74a431aea48bc2709f89
wifeofvillon <upbeatxxx+wifeogvillon@gmail.com>
2017-03-21 23:48:13 +0900

    PM commit
    On first release, I don't use es6 and React.

---
+++ bunal/scoresheet/js/master.js
@@ -0,0 +1,42 @@
+var master = {
+  "character": [
+    {
+      "id": 10,
+      "name": "島崎藤村",
+      "reward":[
+        {
+          "score": 10000,
+          "item": "島崎藤村第四衣装"
+        }
+      ]
+    },
+    {
+      "id": 11,
+      "name": "田山花袋",
+      "reward":[]
+    },
+    {
+      "id": 38,
+      "name": "国木田独歩",
+      "reward":[]
+    },
+  ],
+  "graphfill": [
+    {
+      "border": 40,
+      "color": "#e57373"
+    },
+    {
+      "border": 80,
+      "color": "#ffd54f"
+    },
+    {
+      "border": 100,
+      "color": "#8bc34a"
+    },
+    {
+      "border": 101,
+      "color": "#4db6ac"
+    }
+  ]
+};
