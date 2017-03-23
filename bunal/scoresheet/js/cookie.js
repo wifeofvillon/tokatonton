e04b406d259530a6a692757215ca03fc871c7eca
wifeofvillon <upbeatxxx+wifeogvillon@gmail.com>
2017-03-23 21:43:22 +0900

    Today's last commit
    On Chrome, I cannot use cookie offline.

--- bunal/scoresheet/js/cookie.js
+++ bunal/scoresheet/js/cookie.js
@@ -1,14 +1,51 @@
 const wovKey = "wovbunalsc";
-const wovExpires = "Fri, 23 Mar 2018 02:30:30 GMT";
+const wovExpires = 365;
+const wovCookieProp = {expires:wovExpires};
 
 /**
  * Class SCCookie
- *
+ * depend on jQuery.cookie
+ * @const key - String
+ * @const prop - Object
  */
-var SCCookie = function(){
+var SCCookie = function(key, prop){
   $.cookie.json = true;
+  this.key = key;
+  this.prop = prop;
 
-  var read = function(key){
-    
+  /**
+   * SCCookie.read
+   * @param key - String
+   * @return  value - String
+   */
+  this.read = function(){
+    return $.cookie(this.key);
   };
+
+  /**
+   * SCCookie.write
+   * @param key - String
+   * @param value - String or json
+   * @param prop  - Object
+   * @return
+   */
+  this.write = function(value){
+    return $.cookie(this.key, value, this.prop);
+  };
+
+  /**
+   * SCCookie.remove
+   * @param key - String
+   */
+  this.remove = function(){
+    return $.removeCookie(this.key);
+  }
 };
+
+// test
+var testScc = new SCCookie('testCookieKey',{expires:1});
+console.log(testScc.read());
+console.log(testScc.write('testCookieString'));
+console.log(testScc.read());
+console.log(testScc.write({"key":"value"}));
+console.log(testScc.read());
