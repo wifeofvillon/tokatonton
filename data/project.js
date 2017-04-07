0890c308db56f78f0930521678fed4805b14c52a
wifeofvillon <upbeatxxx+wifeofvillon@gmail.com>
2017-04-07 23:05:54 +0900

    initial commit
    add GA-tracking code

---
+++ data/project.js
@@ -0,0 +1,26 @@
+const projectList = {
+  "category": [
+    {
+      "name": "「文豪とアルケミスト」関連(非公式)",
+      "product": [
+        {
+          "name": "【非公式】文アル「学問ノススメ」成績表",
+          "url": "./bunal/scoresheet/",
+          "image": "",
+          "desc": "「学問ノススメ」イベント支援Webアプリケーション"
+        }
+      ]
+    },
+    {
+      "name": "自作ブックマークレット",
+      "product": [
+          {
+            "name": "GetPageTitleAndURL",
+            "url": "https://github.com/wifeofvillon/WoVBookmarklets/tree/master/GetPageTitleAndURL",
+            "image": "",
+            "desc": "表示中のWebページのURLとタイトルをリンクタグ形式で取得する"
+          }
+      ]
+    }
+  ]
+};
